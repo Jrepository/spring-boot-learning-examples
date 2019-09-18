07c46fb5bb05cb3a229e276ecf3071a7b5687903
zy <>
2019-09-18 16:19:45 +0800

    Spring Boot 入门

---
+++ spring-boot-simple/src/main/java/indi/com/SpringBootSimpleApplication.java
@@ -0,0 +1,11 @@
+package indi.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSimpleApplication.class,args);
+    }
+}
