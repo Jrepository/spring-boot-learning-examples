5500c2ce114c1c46689c721d8ca59f890f3a7b37
zy <>
2019-10-09 17:21:58 +0800

    Spring Boot 拦截器

--- spring-boot-interceptor/src/main/java/indi/com/controller/MyController.java
+++ spring-boot-interceptor/src/main/java/indi/com/controller/MyController.java
@@ -1,12 +1,9 @@
 package indi.com.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 @RestController
 public class MyController {
